2f4cdf9b72ca4b20650d4dac336537c7dab0b0ab
antko35 <kovsheranton@gmail.com>
2024-07-20 13:43:48 +0300

    install identity

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eafaf86c0687f5c50a9b8c776012416cc51029b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7942aa80c399ec283f78646995a34a5615a2b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
