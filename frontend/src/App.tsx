923d33286d38c8abfe44f0dc97a450486fe43a39
antko35 <kovsheranton@gmail.com>
2024-07-29 19:21:21 +0300

    forms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,35 +6,42 @@ import Search from './Components/Search/Search';
 import { CompanySearch } from './company';
 import { searchCompanies } from './api';
 import { Server } from 'http';
+import AddPortfolio from './Components/Portfolio/AddPortfolio/AddPortfolio';
 
 function App() {
   const[search,setSearch] = useState<string>("");
   const[searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const[serverError, setServerError] = useState<string>("");
+  const[serverError, setServerError] = useState<string | null>(null);
 
-    const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
+    const handleSearchChange = (e : ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
-        console.log(e);
+        //console.log(e);
     };
 
-    const onClick = async (e : SyntheticEvent)=>{
-      
-      const result = await searchCompanies(search);
+    const onPortfolioCreate = (e : SyntheticEvent) => {
+      e.preventDefault();
+      console.log(e);
+    }
 
+    const onSearchSubmit = async (e : SyntheticEvent)=>{
+      e.preventDefault();
+      const result = await searchCompanies(search);
       if(typeof result === "string"){
         setServerError(result);
       } else if(Array.isArray(result.data)){
         setSearchResult(result.data);
       }
-
-      console.log(searchResult);
+      //console.log(searchResult);
     };
 
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange} />
+      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange} />
       {serverError && <h1>{serverError}</h1>}
-      <CardList searchResult={searchResult} />
+      <CardList 
+        searchResult={searchResult} 
+        onPortfolioCreate={onPortfolioCreate}
+        />
     </div>
   );
 }
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -2,17 +2,22 @@ import { on } from 'process';
 import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent, FormEvent } from 'react'
 
 interface Props {
-  onClick : (e : SyntheticEvent) => void;
-  search: string|undefined;
-  handleChange : (e : ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit : (e : SyntheticEvent) => void;
+  search: string | undefined;
+  handleSearchChange : (e : ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Search : React.FC<Props> = ({onClick, search, handleChange}: Props) : JSX.Element => {
+const Search : React.FC<Props> = ({
+  onSearchSubmit, 
+  search, 
+  handleSearchChange
+}: Props) : JSX.Element => {
   return (
-    <div>
-        <input value={search} onChange={(e)=> handleChange(e)}></input>
-        <button onClick={(e) => onClick(e)}></button>
-    </div>
+    <>
+      <form onSubmit={onSearchSubmit}>
+        <input value={search} onChange={handleSearchChange} />
+      </form> 
+    </>
   )
 }
 
