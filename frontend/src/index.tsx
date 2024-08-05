96145ab6de5e422c4d4925f450fce74c25801a5b
antko35 <kovsheranton@gmail.com>
2024-08-06 00:34:05 +0300

    Revert "хуйня полная"
    This reverts commit 3344170d085c129063cf265b1beb1b29f163dcee.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './Routes/Routes';
-import App from "./App";
- 
+import { searchCompanies } from './api';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
+console.log(searchCompanies("tsla"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
